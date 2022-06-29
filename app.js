14bdd8fe1b4db63ec0132ee9f9f287c036972bb7
rafatalamsaifi <rafatalamsaifi@gmail.com>
2022-06-29 10:35:20 +0000

    setname

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
 const app = express();
-const port = 80;
+const port = 8000;
+
+app.use('/static', express.static('static'));
+app.use(express.urlencoded());
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res)=>{
+    const params = { }
+    res.status(200).render('index.pug', params)
+
+})
+
+app.listen(port, ()=>{
+    console.log(`This application started successfully on port ${port}`)
+});
