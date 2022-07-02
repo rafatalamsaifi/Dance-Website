621c16e68012f4bdf01c0952a67421f2ff30b25b
rafatalamsaifi <rafatalamsaifi@gmail.com>
2022-07-02 07:13:20 +0000

    for pull repository

--- app.js
+++ app.js
@@ -11,7 +11,19 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res)=>{
     const params = { }
-    res.status(200).render('index.pug', params)
+    res.status(200).render('home.pug', params)
+
+});
+
+app.get('/contact', (req, res)=>{
+    const params = { }
+    res.status(200).render('contact.pug', params)
+
+})
+
+app.get('/about', (req, res)=>{
+    const params = { }
+    res.status(200).render('about.pug', params)
 
 })
 
